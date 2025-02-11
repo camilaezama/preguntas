9adaf22baaf54cfe1a7bcad613bc8eda17972a9f
camilaezama <camilaezama2000@gmail.com>
2025-02-11 14:03:19 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,426 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MessiQuizApp());
+}
+
+class MessiQuizApp extends StatelessWidget {
+  const MessiQuizApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const IntroScreen(),
+    );
+  }
+}
+
+class IntroScreen extends StatefulWidget {
+  const IntroScreen({super.key});
+
+  @override
+  _IntroScreenState createState() => _IntroScreenState();
+}
+
+class _IntroScreenState extends State<IntroScreen> {
+  int step = 0;
+
+  // Lista de mensajes con su respectiva imagen (si corresponde)
+  final List<Map<String, dynamic>> messages = [
+    {
+      "text": "Hola Hermoso, felices 6 años!!!",
+      "image": "assets/jani.jpg",
+    },
+    {
+      "text": "Te amo mucho",
+      "image": "assets/linda1.jpg",
+    },
+    {
+      "text":
+          "Se que estas esperando la camiseta....",
+      "image": null,
+    },
+    {
+      "text":
+          "pero la vida no es facil, asi que te la vas a tener que ganar",
+      "image": null,
+    },
+    {
+      "text":
+          "Para eso tenes que aprobar este examen",
+      "image": null,
+    },
+    {
+      "text":
+          "Y el tema que te toco es: San lorenzo",
+      "image": "assets/sl2.jpg",
+    },
+    {
+      "text":
+          "JAJA ya quisieras",
+      "image": null,
+    },
+    {
+      "text":
+          "es sobre NOSOTROS :)",
+      "image": null,
+    },
+    {
+      "text": "Si messi pudo ganar el mundial vos podes ganar este juego!!!",
+      "image": "assets/correct_answer.jpg",
+    },
+    {
+      "text": "Si desaprobas tu camiseta sera vendida en Mercado Libre",
+      "image": "assets/meli.jpg",
+    },
+    {
+      "text": "Suerte, te amo!!!!",
+      "image": "assets/linda2.jpg",
+    },
+  ];
+
+  // Función para pasar al siguiente mensaje
+  void nextStep() {
+    if (step < messages.length - 1) {
+      setState(() {
+        step++;
+      });
+    } else {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const QuizScreen()),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueAccent,
+      body: GestureDetector(
+        onTap: nextStep, // Detectar clic en cualquier parte de la pantalla
+        child: Container(
+          // Usar un Container que ocupe toda la pantalla
+          width: double.infinity,
+          height: double.infinity,
+          color: const Color.fromARGB(255, 133, 158, 202), // Asegurarse de que el fondo cubra toda la pantalla
+          child: Center(
+            child: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 500),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Mostrar el texto del mensaje actual
+                  Text(
+                    messages[step]['text'],
+                    key: ValueKey(step),
+                    style: const TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 20),
+                  // Mostrar la imagen si está presente
+                  if (messages[step]['image'] != null)
+                    Image.asset(
+                      messages[step]['image']!,
+                      width: 400,
+                      height: 400,
+                    ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizScreen extends StatefulWidget {
+  const QuizScreen({super.key});
+
+  @override
+  _QuizScreenState createState() => _QuizScreenState();
+}
+
+class _QuizScreenState extends State<QuizScreen> {
+  int questionIndex = 0;
+  int correctAnswers = 0;
+
+  // Controlador para el campo de texto en preguntas de completar
+  final TextEditingController _completarController = TextEditingController();
+
+  final List<Map<String, dynamic>> questions = [
+    {
+      "question": "1. Cual fue la primera comida que comapartimos juntos?",
+      "options": ["El club de la milanesa", "La muzza", "Mc Donalds", "Barley"],
+      "answer": "Mc Donalds",
+      "type": "opcionesMultiples",
+      "questionImage": null,
+      "image": "assets/chile-nieve.jpg",
+      "correctMessage": "¡Correcto! Todo empezo en el mc de guemes. Vos no te terminaste la hamburguesa",
+      "incorrectMessage": "Incorrecto... fuimos al mc. Arrancamos mal",
+    },
+    {
+      "question": "2. En que año viajamos a Chile?",
+      "options": ["2020", "2021", "2022", "2023"],
+      "answer": "2022",
+      "type": "opcionesMultiples",
+      "questionImage": null,
+      "image": "assets/chile-nieve.jpg",
+      "correctMessage": "¡Correcto! Fuimos en 2022. Sos bueno para los años",
+      "incorrectMessage": "Incorrecto... La respuesta correcta es: 2022.",
+    },
+    {
+      "question": "2. En que cerro de Chile comimos el mote con huesillo?",
+      "options": ["Cerro Santa Lucía", "Cerro San Cristóbal", "Cerro Manquehue", "Cerro Leonera"],
+      "answer": "Cerro San Cristóbal",
+      "type": "opcionesMultiples",
+      "questionImage": "assets/chile-comida.jpg",
+      "image": "assets/san-cristobal.jpg",
+      "correctMessage": "¡Correctoooo! Esa pense que le errabas",
+      "incorrectMessage": "Incorrecto... era el San Cristobal. No me putees",
+    },
+    {
+      "question": "3. Como se llama la cancion de Camilo que arranca con 'tu debes ser ingeniera...'?",
+      "options": ["Vida de rico", "La boda", "La mitad", "Millones"],
+      "answer": "Millones",
+      "type": "opcionesMultiples",
+      "questionImage": null,
+      "image": "assets/camilo.jpg",
+      "correctMessage": "¡Bieeeen! Cantame un poquito...",
+      "incorrectMessage": "Nooooo... hay que practicar para el concierto",
+    },
+    {
+      "question": "Completa: En la final, Messi hizo __ goles.",
+      "answer": "2",
+      "type": "completar",
+      "questionImage": "assets/messi_goles.jpg", // Imagen para la pregunta
+      "image": "assets/messi_completando.jpg", // Imagen para la respuesta
+      "correctMessage": "¡Correcto! Messi hizo 2 goles.",
+      "incorrectMessage": "Incorrecto... La respuesta correcta es: 2.",
+    }
+  ];
+
+  void checkAnswer(BuildContext context, dynamic selectedAnswer) {
+    final correct = questions[questionIndex]['answer'];
+    bool isCorrect;
+
+    // Si la pregunta es de tipo 'completar', comparamos el texto ingresado
+    if (questions[questionIndex]['type'] == 'completar') {
+      isCorrect =
+          selectedAnswer.trim() == correct.trim(); // Comparación de texto
+    } else {
+      isCorrect = selectedAnswer == correct;
+    }
+
+    if (isCorrect) correctAnswers++;
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => AnswerScreen(
+          isCorrect: isCorrect,
+          onNext: () {
+            if (questionIndex < questions.length - 1) {
+              setState(() {
+                questionIndex++;
+              });
+              Navigator.pop(context);
+            } else {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => ResultScreen(correctAnswers)),
+              );
+            }
+          },
+          correctAnswer: correct,
+          imagePath: questions[questionIndex]['image'],
+          correctMessage: questions[questionIndex]['correctMessage'],
+          incorrectMessage: questions[questionIndex]['incorrectMessage'],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final currentQuestion = questions[questionIndex];
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 255, 152, 120),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [            
+            Text(
+              currentQuestion["question"],
+              style: const TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            // Mostrar imagen de la pregunta
+            if (currentQuestion["questionImage"] != null)
+              Image.asset(
+                currentQuestion["questionImage"],
+                width: 200,
+                height: 200,
+              ),
+            const SizedBox(height: 20),
+            if (currentQuestion["type"] == "opcionesMultiples")
+              ...currentQuestion["options"]
+                  .map<Widget>((option) => Padding(
+                    padding: const EdgeInsets.only(bottom: 10.0),
+                    child: ElevatedButton(
+                          onPressed: () => checkAnswer(context, option),
+                          child: Text(option),
+                        ),
+                  )),
+            if (currentQuestion["type"] == "completar")
+              Column(
+                children: [
+                  SizedBox(
+                    width: 250,
+                    child: TextField(
+                      controller: _completarController, // Usamos el controlador
+                      decoration: const InputDecoration(
+                        hintText: "Escribí tu respuesta",
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(),
+                      ),
+                      onSubmitted: (value) => checkAnswer(context, value),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Obtenemos el valor ingresado en el TextField
+                      String userAnswer = _completarController.text;
+                      checkAnswer(context, userAnswer);
+                    },
+                    child: const Text("Enviar respuesta"),
+                  ),
+                ],
+              ),
+            if (currentQuestion["type"] == "ordenar")
+              Column(
+                children: [
+                  const Text("Ordena los elementos correctamente",
+                      style: TextStyle(color: Colors.white)),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () => checkAnswer(context,
+                        ["Primer gol", "Segundo gol", "Tanda de penales"]),
+                    child: const Text("Enviar respuesta"),
+                  ),
+                ],
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AnswerScreen extends StatelessWidget {
+  final bool isCorrect;
+  final VoidCallback onNext;
+  final dynamic correctAnswer;
+  final String imagePath;
+  final String correctMessage;
+  final String incorrectMessage;
+
+  const AnswerScreen({super.key, 
+    required this.isCorrect,
+    required this.onNext,
+    required this.correctAnswer,
+    required this.imagePath,
+    required this.correctMessage,
+    required this.incorrectMessage,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    String message = isCorrect ? correctMessage : incorrectMessage;
+
+    return Scaffold(
+      backgroundColor: isCorrect ? Colors.green : Colors.red,
+      body: GestureDetector(
+        onTap: onNext,
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          color: isCorrect ? Colors.green : Colors.red,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  message,
+                  style: const TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 20),
+                Image.asset(imagePath, width: 200, height: 200),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ResultScreen extends StatelessWidget {
+  final int correctAnswers;
+
+  const ResultScreen(this.correctAnswers, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.purple,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "¡Te ganaste la camiseta, felicitaciones! 🎉",
+              style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 20),
+            Text(
+              "Respondiste $correctAnswers de 3 preguntas correctamente.",
+              style: const TextStyle(fontSize: 18, color: Colors.white),
+            ),
+            const SizedBox(height: 20),
+            const Icon(Icons.sports_soccer, size: 100, color: Colors.white),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () =>
+                  Navigator.popUntil(context, (route) => route.isFirst),
+              child: const Text("Jugar de nuevo"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
