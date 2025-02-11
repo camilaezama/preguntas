2bc0834d54e7352f6f5ce9aef433b318596b7b26
camilaezama <camilaezama2000@gmail.com>
2025-02-11 14:32:38 -0300

    build

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -45,7 +45,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "5fc0363b6c7450c7256a68b3dd931b6a
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "6396930ee4b013f24d9fb4e0e2b5f5f2",
+"flutter_bootstrap.js": "bb8e2c0d5e931c2a6d9e2adf2abe0d04",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
